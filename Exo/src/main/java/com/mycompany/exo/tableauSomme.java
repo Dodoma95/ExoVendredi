1ba5c1f0c7a07d00861b8628def0f2b966cdf96d
dorian <maillard.dorian60@gmail.com>
2019-10-25 10:17:33 +0200

    second commit

---
+++ Exo/src/main/java/com/mycompany/exo/tableauSomme.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exo;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class tableauSomme {
+    
+       public static void main(String[] args) {
+           System.out.println("Calculsomme");
+    }
+ 
+    
+}
+   

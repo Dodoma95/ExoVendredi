b908826e4b9be448ef6973d9e4dcb0aeab992404
dorian <maillard.dorian60@gmail.com>
2019-10-25 11:54:00 +0200

    quatrieme commit

---
+++ Exo/src/main/java/com/mycompany/exo/DessinEtoile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exo;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class DessinEtoile {
+    
+    public static String dessinEtoile() {
+    String etoiles = "*\n" + "**\n" + "***\n" + "****\n" + "*****\n" + "******\n";
+   
+    return etoiles;
+}   
+    public static void main(String[] args) {
+        String etoiles = "*\n" + "**\n" + "***\n" + "****\n" + "*****\n" + "******\n";
+        System.out.println(etoiles);
+    }
+}
